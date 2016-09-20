b87b3100b4967a538731f6639c74ce15a751f4c4
localadmin <localadmin@cloud9.local>
2016-09-20 14:52:49 +0100

    hacky commit

--- index.js
+++ index.js
@@ -100,16 +100,29 @@ var findOrCreateSession = function findOrCreateSession (fbid) {
 
 // Our bot actions
 const actions = {
-  send(request, response) {
-    const {sessionId, context, entities} = request
-    const {text, quickreplies} = response
-    return new Promise(function (resolve, reject) {
-      console.log('sending...', JSON.stringify(response))
-      console.log('sending it to..%s', JSON.stringify(request))
-      fbMessage(request[0].fbid, JSON.stringify(response)).catch(console.error)
-
-      return resolve()
-    })
+  send({sessionId}, {text}) {
+    // Our bot has something to say!
+    // Let's retrieve the Facebook user whose session belongs to
+    const recipientId = sessions[sessionId].fbid
+    if (recipientId) {
+      // Yay, we found our recipient!
+      // Let's forward our bot response to her.
+      // We return a promise to let our bot know when we're done sending
+      return fbMessage(recipientId, text)
+        .then(() => null)
+        .catch((err) => {
+          console.error(
+            'Oops! An error occurred while forwarding the response to',
+            recipientId,
+            ':',
+            err.stack || err
+          )
+        })
+    } else {
+      console.error("Oops! Couldn't find user for session:", sessionId)
+      // Giving the wheel back to our bot
+      return Promise.resolve()
+    }
   },
 
   getVouchers({context, entities}) {
