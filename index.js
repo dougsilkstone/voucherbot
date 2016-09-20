c00a0d015f81b1f3c8e8ceee1020cbbbda41d7f0
localadmin <localadmin@cloud9.local>
2016-09-20 14:39:55 +0100

    Added fbid

--- index.js
+++ index.js
@@ -105,7 +105,7 @@ const actions = {
     const {text, quickreplies} = response
     return new Promise(function (resolve, reject) {
       console.log('sending...', JSON.stringify(response))
-      fbMessage(sessionId, JSON.stringify(response)).catch(console.error)
+      fbMessage(sessionId.fbid, JSON.stringify(response)).catch(console.error)
 
       return resolve()
     })
